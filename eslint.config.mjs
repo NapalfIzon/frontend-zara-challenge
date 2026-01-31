6bc08f800d6a195881a0ef90bb48b367cd226041
Napoleon Izquieta <napoleon.izquieta@promocionesfarma.com>
2026-01-31 02:07:33 +0100

    📝 Fixing dependencies and config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,37 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "dist/**",
-    "coverage/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'dist/**', 'coverage/**', 'next-env.d.ts']),
   {
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     rules: {
-      "no-console": process.env.ENVIRONMENT === "production" ? "error" : "off",
-      "no-debugger": process.env.ENVIRONMENT === "production" ? "error" : "off",
-      "prefer-const": "error",
-      "unused-imports/no-unused-imports": "error",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
+      'no-console': process.env.ENVIRONMENT === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.ENVIRONMENT === 'production' ? 'error' : 'off',
+      'prefer-const': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
+    plugins: { 'unused-imports': unusedImports },
+    ignores: ['node_modules/**', 'public/**', 'dist/**', 'coverage/**', '.next/**'],
   },
 ]);
 
